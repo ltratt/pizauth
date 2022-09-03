2c23ce252623dd52d2e9dcf28e9d484dd326b7a1
Laurence Tratt <laurie@tratt.net>
2022-09-03 15:27:47 +0100

    Don't force a rebuild if non-source files are changed.
    This tends to particularly confuse me when I've just changed the
    README...

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
 use cfgrammar::yacc::YaccKind;
 use lrlex::{CTLexerBuilder, DefaultLexeme};
+use rerun_except::rerun_except;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    rerun_except(&["LICENSE-APACHE", "LICENSE-MIT", "README.md"])?;
+
     CTLexerBuilder::<DefaultLexeme<u8>, u8>::new_with_lexemet()
         .lrpar_config(|ctp| {
             ctp.yacckind(YaccKind::Grmtools)
