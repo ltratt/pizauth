2999d6a3268fadf76bc1f6f14fcc4aedb9a05b3a
Laurence Tratt <laurie@tratt.net>
2022-09-06 12:33:56 +0100

    Reset the tokenstate to Empty for accounts that have changed on reload.
    This is an important security property: if the user changes an account's
    details in `pizauth.conf` we can't know if the altered account is really
    "the same" or not. So if any aspects of the account alter, we ensure the
    tokenstate is set to `Empty`. In other words, reloading config can cause
    you to have to reauthenticate an account. This is a more acceptable
    outcome than leaking tokens!

--- src/server/user_listener.rs
+++ src/server/user_listener.rs
@@ -1,22 +1,42 @@
 use std::{collections::HashMap, error::Error, path::Path, sync::Arc};
 
+use log::info;
+
 use super::{AuthenticatorState, TokenState};
 use crate::Config;
 
 pub fn reload_conf(pstate: Arc<AuthenticatorState>, conf_path: &str) -> Result<(), Box<dyn Error>> {
     let new_conf = Config::from_path(Path::new(conf_path))?;
     let mut ct_lk = pstate.ct_lock();
+    let mut resets = Vec::new();
     let new_tokens = new_conf
         .accounts
         .iter()
-        .map(|(k, _)| {
-            let tokstate = match ct_lk.validate_act_name(k) {
-                Some(act_id) => ct_lk.tokenstate(&act_id).clone(),
-                None => TokenState::Empty,
+        .map(|(act_name, act)| {
+            let tokstate = match ct_lk.validate_act_name(act_name) {
+                // If the account already exists and if the account hasn't changed any of its
+                // details, we can keep the tokenstate unchanged.
+                Some(act_id) if ct_lk.account(&act_id) == act.as_ref() => {
+                    ct_lk.tokenstate(&act_id).clone()
+                }
+                // If the account already exists, some details of the account have changed, and
+                // the tokenstate isn't `Empty`, then we warn the user that we're going to reset
+                // things. [If the tokenstate is `Empty`, the warning is superfluous.]
+                Some(act_id) if !matches!(ct_lk.tokenstate(&act_id), TokenState::Empty) => {
+                    resets.push(format!(
+                        "Account {} has changed some of its details: resetting token state entirely",
+                        ct_lk.account(&act_id).name));
+                    TokenState::Empty
+                }
+                _ => TokenState::Empty,
             };
-            (k.to_owned(), tokstate)
+            (act_name.to_owned(), tokstate)
         })
         .collect::<HashMap<_, _>>();
     ct_lk.update((new_conf, new_tokens));
+    drop(ct_lk);
+    for msg in resets {
+        info!("{}", msg);
+    }
     Ok(())
 }
